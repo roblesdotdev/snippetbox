e52d770d9f26c978a58c79f5d09715ebddf9740a
roblesdotdev <httpsnip@gmail.com>
2024-01-11 21:23:47 -0300

    fix: fmt

--- main.go
+++ main.go
@@ -8,12 +8,12 @@ import (
 // Define a home handler function which writes a byte slice containing
 // "Hello from Snippetbox" as the response body.
 func home(w http.ResponseWriter, r *http.Request) {
-  // Check if the current request URL path exactly matches "/". If it doesn't, use
-  // the http.NotFound() function to send a 404 response to the client.
-  if r.URL.Path != "/" {
-    http.NotFound(w, r)
-    return
-  }
+	// Check if the current request URL path exactly matches "/". If it doesn't, use
+	// the http.NotFound() function to send a 404 response to the client.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 
 	w.Write([]byte("Hello from Snippetbox"))
 }
